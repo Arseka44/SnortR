3f8198d91282c9ac94cd839960ac6c663d117e40
Aleksander <petr0vskjy.aleksander@gmail.com>
2017-06-07 12:14:28 +0400

    Update global.R

--- global.R
+++ global.R
@@ -1,3 +1,8 @@
+# --------------------------------------------
+# Traffic analysis
+# petr0vskjy.aleksander@gmail.com
+# Global function`s
+# -------------------------------------------
 rm(list=ls())
 library(shiny)
 library(shinydashboard)
@@ -7,9 +12,11 @@ library(dplyr)
 library(iptools)
 library(crafter)
 library(DT)
-#source("get_traffic.R")
-# read traffic
+# -----------------------------------------------------------
+# read sample traffic from local drive 
+# more about traffic source look here https://github.com/petr0vsk/SnortR/wiki/Publicly-available-PCAP-files
 project.dir <- "/root/Documents/SnortR/TrafficExamples"
+
 stopifnot( dir.exists(file.path(project.dir))  )
 setwd(file.path(project.dir))
 traffic.all <- read.table(file="CTF_1.txt", quote="\"", header = T, sep=",")
